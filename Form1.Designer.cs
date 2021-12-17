e419c5780e55197798f3033b617827c1c3425d25
KevCodes <41352514+kevman5@users.noreply.github.com>
2021-12-17 17:44:46 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace MethodOne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mass = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_1 = new System.Windows.Forms.TextBox();
+            this.tb_2 = new System.Windows.Forms.TextBox();
+            this.tb_ans1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_ans2 = new System.Windows.Forms.TextBox();
+            this.tb_4 = new System.Windows.Forms.TextBox();
+            this.tb_3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Mass
+            // 
+            this.Mass.AutoSize = true;
+            this.Mass.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Mass.ForeColor = System.Drawing.Color.Red;
+            this.Mass.Location = new System.Drawing.Point(63, 35);
+            this.Mass.Name = "Mass";
+            this.Mass.Size = new System.Drawing.Size(51, 15);
+            this.Mass.TabIndex = 0;
+            this.Mass.Text = "Length 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(66, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Length 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(66, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Distance";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(95, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_1
+            // 
+            this.tb_1.Location = new System.Drawing.Point(124, 35);
+            this.tb_1.Name = "tb_1";
+            this.tb_1.Size = new System.Drawing.Size(100, 20);
+            this.tb_1.TabIndex = 4;
+            // 
+            // tb_2
+            // 
+            this.tb_2.Location = new System.Drawing.Point(124, 83);
+            this.tb_2.Name = "tb_2";
+            this.tb_2.Size = new System.Drawing.Size(100, 20);
+            this.tb_2.TabIndex = 5;
+            // 
+            // tb_ans1
+            // 
+            this.tb_ans1.Location = new System.Drawing.Point(124, 168);
+            this.tb_ans1.Name = "tb_ans1";
+            this.tb_ans1.Size = new System.Drawing.Size(100, 20);
+            this.tb_ans1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(391, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Angular Velocity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(391, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Radius";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label5.Location = new System.Drawing.Point(391, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Tangential Velocity";
+            // 
+            // tb_ans2
+            // 
+            this.tb_ans2.Location = new System.Drawing.Point(529, 174);
+            this.tb_ans2.Name = "tb_ans2";
+            this.tb_ans2.Size = new System.Drawing.Size(100, 20);
+            this.tb_ans2.TabIndex = 10;
+            // 
+            // tb_4
+            // 
+            this.tb_4.Location = new System.Drawing.Point(529, 83);
+            this.tb_4.Name = "tb_4";
+            this.tb_4.Size = new System.Drawing.Size(100, 20);
+            this.tb_4.TabIndex = 11;
+            // 
+            // tb_3
+            // 
+            this.tb_3.Location = new System.Drawing.Point(529, 38);
+            this.tb_3.Name = "tb_3";
+            this.tb_3.Size = new System.Drawing.Size(100, 20);
+            this.tb_3.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button2.Location = new System.Drawing.Point(446, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 41);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BurlyWood;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tb_3);
+            this.Controls.Add(this.tb_4);
+            this.Controls.Add(this.tb_ans2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_ans1);
+            this.Controls.Add(this.tb_2);
+            this.Controls.Add(this.tb_1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Mass);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Mass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_1;
+        private System.Windows.Forms.TextBox tb_2;
+        private System.Windows.Forms.TextBox tb_ans1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_ans2;
+        private System.Windows.Forms.TextBox tb_4;
+        private System.Windows.Forms.TextBox tb_3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MethodOne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void CalcpythagTheoreom()
+        {
+            int a2, b2, c2;
+            a2 = int.Parse(tb_1.Text);
+            b2 = int.Parse(tb_2.Text);
+            c2 = (a2 * a2) + (b2 * b2);
+            tb_ans1.Text = c2.ToString();
+        }
+        public void CalcPower()
+        {
+            int power, time, work;
+            work = int.Parse(tb_3.Text);
+            time = int.Parse(tb_4.Text);
+            power = work / time;
+
+            tb_ans2.Text = power.ToString();
+        }
+        public void CalcTensileStress()
+        {
+            int stress1, force1, area1;
+            force1 = int.Parse(tb_1.Text);
+            area1 = int.Parse(tb_2.Text);
+            stress1 = force1 / area1;
+
+            tb_ans1.Text = stress1.ToString();
+        }
+        public void CalcTangVeloc()
+        {
+            double rad1, angv1, tang1;
+            rad1 = double.Parse(tb_3.Text);
+            angv1 = double.Parse(tb_4.Text);
+            tang1 = rad1 * angv1;
+
+            tb_ans2.Text = tang1.ToString();
+        }
+        public void CalcWeight()
+        {
+            double mass1, grav1, weight1;
+            mass1 = double.Parse(tb_1.Text);
+            grav1 = double.Parse(tb_2.Text);
+            weight1 = mass1 * grav1;
+            tb_ans1.Text = weight1.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalcpythagTheoreom();
+            
+            // Make sure to update label names if you change methods!!
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CalcTangVeloc();
+            // Make sure to update label names if you change methods!!
+        }
+    }
+}
